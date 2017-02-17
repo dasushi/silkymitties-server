971ec61a9a56e6cf7753221409fd009a7d2b6dc0
Stephen A <stephen.arsenault17@gmail.com>
2017-02-16 20:06:21 -0500

    Added databases and access for raw, fused, ml shots

--- core/bottleapp.py
+++ core/bottleapp.py
@@ -7,43 +7,31 @@ mongo = MongoPlugin(uri='mongodb://localhost:27017', db='restdb', json_mongo=Tru
 app.install(mongo)
 
 
-SENSORTYPE = (
-    ('a', 'ACCEL'),
-    ('g', 'GYRO'),
-    ('f', 'FUSION'),
-)
-
-HANDEDNESS = (
-    ('l', 'LEFT'),
-    ('r', 'RIGHT'),
-)
-
-
 ### RAWSHOTS ###
 #Stores raw copies of shots directly from Android device
-@app.route('/silkymitties/rawshots', methods=['GET'])
+@app.route('/rawshots', methods=['GET'])
 def getAllShots(mongodb):
-    shots = mongodb['shots']
+    shots = mongodb['rawshots']
     output = []
     for s in shots.find():
         output.append(dumps(s))
-    return dumps({'Shots': output})
+    return dumps({'rawshots': output})
 
-@app.route('/silkymitties/rawshot/<shot_id:int>', methods=['GET'])
+@app.route('/rawshot/<shot_id:int>', methods=['GET'])
 def getShot(shot_id, mongodb):
-    shot = mongodb['shots'].find({'_id':shot_id})
-    return dumps({'Shot': shot})
+    shot = mongodb['rawshots'].find({'_id':shot_id})
+    return dumps({'RawShot': shot})
 
-@app.route('/silkymitties/shot/<shot_id:int>', methods=['DELETE'])
+@app.route('/rawshot/<shot_id:int>', methods=['DELETE'])
 def deleteShot(shot_id, mongodb):
-    mongodb['shots'].remove({'_id':shot_id}, 1)
+    mongodb['rawshots'].remove({'_id':shot_id}, 1)
     return dumps({'Result': True})
 
-@app.route('/silkymitties/rawshots', methods=['POST'])
+@app.route('/rawshots', methods=['POST'])
 def createShot(mongodb):
     if not request.json:
         abort(400)
-    shots = mongodb['shots']
+    shots = mongodb['rawshots']
     userID = request.json['userID']
     upperAccel = request.json['upperAccel']
     upperGyro = request.json['upperGyro']
@@ -52,28 +40,60 @@ def createShot(mongodb):
     new_shot_id = shots.insert({'userID':userID, 'upperGyro':upperGyro, 'upperAccel':upperAccel, 'lowerGyro':lowerGyro, 'lowerAccel':lowerAccel}).inserted_id
     new_shot = shots.find({'_id':new_shot_id})
 
-    return dumps({'shotID': new_shot_id, 'userID':userID}), 201
+    return dumps({'shotID': new_shot_id}), 201
 
 ### FUSED SHOTS ###
+@app.route('/fusedshots', methods=['GET'])
+def getAllShots(mongodb):
+    shots = mongodb['fusedshots']
+    output = []
+    for s in shots.find():
+        output.append(dumps(s))
+    return dumps({'rawshots': output})
+
+@app.route('/fusedshot/<shot_id:int>', methods=['GET'])
+def getShot(shot_id, mongodb):
+    shot = mongodb['fusedshots'].find({'_id':shot_id})
+    return dumps({'RawShot': shot})
+
+@app.route('/fusedshot/<shot_id:int>', methods=['DELETE'])
+def deleteShot(shot_id, mongodb):
+    mongodb['fusedshots'].remove({'_id':shot_id}, 1)
+    return dumps({'Result': True})
 
 ### ML_DATA ###
+@app.route('/mlresults', methods=['GET'])
+def getAllShots(mongodb):
+    results = mongodb['mlresults']
+    output = []
+    for s in results.find():
+        output.append(dumps(s))
+    return dumps({'ML Results': output})
 
+@app.route('/mlresult/<shot_id:int>', methods=['GET'])
+def getShot(shot_id, mongodb):
+    shot = mongodb['mlresults'].find({'_id':shot_id})
+    return dumps({'ML Result': shot})
+
+@app.route('/mlresult/<shot_id:int>', methods=['DELETE'])
+def deleteShot(shot_id, mongodb):
+    mongodb['mlresults'].remove({'_id':shot_id}, 1)
+    return dumps({'Result': True})
 
 
 ### USERS ###
 #Database of all Users
-
-@app.route('/silkymitties/user/<user_id:int>', methods=['GET'])
+@app.route('/user/<user_id:int>', methods=['GET'])
 def getUser(user_id, mongodb):
     user = mongodb['users'].find({'_id':user_id})
     return dumps({'User': user})
 
-@app.route('/silkymitties/user/<user_id:int>', methods=['DELETE'])
+@app.route('/user/<user_id:int>', methods=['DELETE'])
 def deleteUser(user_id, mongodb):
     mongodb['users'].remove({'_id':user_id}, 1)
     return dumps({'Result': True})
 
-@app.route('/silkymitties/users', methods=['POST'])
+@app.route('/users', methods=['POST'])
 def createUser(mongodb):
     if not request.json:
         abort(400)
@@ -89,11 +109,11 @@ def createUser(mongodb):
 
 @app.route('/')
 def index():
-    return "The Silkiest Mitties"
+    return "The Silkiest Mitties - APIS: /users, /rawshots, /fusedshots, /mlresult"
 
 @error(404)
 def not_found(error):
     return dumps({'404': 'Resource Not Found'})
 
 if __name__=='__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=80)
